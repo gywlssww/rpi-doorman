c32c008ce9d71d0263f9c6213eb432ef160a31bd
nalbam <me@nalbam.com>
2020-06-09 16:38:05 +0900

    pushd

--- lepton.sh
+++ lepton.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-../LeptonModule/software/raspberrypi_video/raspberrypi_video -mirror -min 20 -max 37
+pushd ~/
+
+./LeptonModule/software/raspberrypi_video/raspberrypi_video -mirror -min 20 -max 37
+
+popd

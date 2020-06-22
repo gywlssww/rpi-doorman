f4a5b1fdb412a27340cfed989ef2f98bf7ab5f39
nalbam <me@nalbam.com>
2020-06-22 14:12:22 +0900

    raspberrypi_video base

--- lepton.sh
+++ lepton.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+LEPTON_BASE=${LEPTON_BASE:-633}
+LEPTON_MIN=${LEPTON_MIN:-12}
+LEPTON_MAX=${LEPTON_MAX:-37}
+
 pushd ~/LeptonModule/software/raspberrypi_video
 
 if [ ! -f raspberrypi_video ]; then
   qmake && make
 fi
 
-./raspberrypi_video -mirror -min 14 -max 37
+./raspberrypi_video -mirror -base ${LEPTON_BASE} -min ${LEPTON_MIN} -max ${LEPTON_MAX}
 
 popd
